d375dcf250d92134aad68f9dc6756f9d9680a27a
melvyn69 <melvynbenichou@gmail.com>
2026-01-18 17:05:18 +0100

    chore(ci): fix lint/ts blockers

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'server/_shared_dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
